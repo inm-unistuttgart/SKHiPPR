f9344532813ddca504a95e2cff9ac50e63d65538
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2025-12-10 15:54:21 +0100

    problem is due to difference in hill matrix

--- examples/pendulum.py
+++ examples/pendulum.py
@@ -23,11 +23,11 @@ def main():
 
     solver = NewtonSolver(tolerance=1e-8, max_iterations=50, verbose=True)
 
-    m = 0.8
+    m = 1
     g = 9.81
     l = 1.0
-    d = 0.5
-    F = 1
+    d = 0.05
+    F = 0.5
     omega = 1.15
     phi = 0.0
 
@@ -39,7 +39,9 @@ def main():
     dae_bad = PendulumDAE(m, d, g, l, F, omega, phi)
 
     # HBM systems
-    fourier_ode = Fourier(N_HBM=N_HBM, L_DFT=1000, n_dof=ode.n_dof)
+    fourier_ode = Fourier(
+        N_HBM=N_HBM, L_DFT=2000, n_dof=ode.n_dof, real_formulation=True
+    )
     hbm_ode = HBMEquation(
         ode,
         omega,
@@ -69,7 +71,7 @@ def main():
     )
 
     fourier_dae = Fourier(
-        N_HBM=5, L_DFT=1000, n_dof=dae_good.n_dof, real_formulation=True
+        N_HBM=3, L_DFT=2000, n_dof=dae_good.n_dof, real_formulation=False
     )
     hbm_dae_good = HBMEquationDAE(
         dae_good,
@@ -90,7 +92,7 @@ def main():
     print("Solved DAE. \n")
 
     fourier_dae = Fourier(
-        N_HBM=6, L_DFT=1000, n_dof=dae_good.n_dof, real_formulation=True
+        N_HBM=4, L_DFT=2000, n_dof=dae_good.n_dof, real_formulation=False
     )
     hbm_dae_bad = HBMEquationDAE(
         dae_bad,
@@ -137,11 +139,89 @@ def main():
     ax.plot(np.cos(phis), np.sin(phis), "gray", label="Unit circle")
     ax.plot(np.real(hbm_ode.eigenvalues), np.imag(hbm_ode.eigenvalues), "x")
     ax.plot(np.real(hbm_dae_good.eigenvalues), np.imag(hbm_dae_good.eigenvalues), "+")
+    ax.plot(np.real(hbm_dae_bad.eigenvalues), np.imag(hbm_dae_bad.eigenvalues), "*")
     ax.set_aspect("equal", "box")
-    ax.set_title(f"Floquet multipliers pendulum N_HBM = {hbm_dae_good.fourier.N_HBM}")
+    ax.set_title(
+        f"Floquet multipliers pendulum N_HBM = {hbm_dae_good.fourier.N_HBM} and {hbm_dae_bad.fourier.N_HBM}"
+    )
 
-    print(hbm_dae_good.eigenvalues)
-    print(hbm_dae_bad.eigenvalues)
+    if hbm_dae_good.fourier.real_formulation:
+        xvals_freqs_good = np.hstack(
+            (
+                np.arange(0, hbm_dae_good.fourier.N_HBM + 1),
+                np.arange(1, hbm_dae_good.fourier.N_HBM + 1),
+            )
+        )
+        xvals_freqs_bad = np.hstack(
+            (
+                np.arange(0, hbm_dae_bad.fourier.N_HBM + 1),
+                np.arange(1, hbm_dae_bad.fourier.N_HBM + 1),
+            )
+        )
+    else:
+        xvals_freqs_good = np.arange(
+            -hbm_dae_good.fourier.N_HBM, hbm_dae_good.fourier.N_HBM + 1
+        )
+        xvals_freqs_bad = np.arange(
+            -hbm_dae_bad.fourier.N_HBM, hbm_dae_bad.fourier.N_HBM + 1
+        )
+
+    X_good = np.reshape(hbm_dae_good.X, shape=(dae_good.n_dof, -1), order="F")
+    X_bad = np.reshape(hbm_dae_bad.X, shape=(dae_bad.n_dof, -1), order="F")
+    hill_matrix_good = hbm_dae_good.hill_matrix()
+    hill_matrix_bad = hbm_dae_bad.hill_matrix()
+
+    fig, axs = plt.subplots(3, 1)
+    axs[0].bar(
+        xvals_freqs_good,
+        np.linalg.norm(X_good, ord=2, axis=0),
+    )
+    axs[0].set_yscale("log")
+    axs[0].set_title("DAE good case")
+    axs[1].bar(
+        xvals_freqs_bad,
+        np.linalg.norm(X_bad, ord=2, axis=0),
+    )
+    axs[1].set_yscale("log")
+    axs[1].set_title("DAE bad case")
+
+    # Remove all additional harmonics
+    if hbm_dae_bad.fourier.real_formulation:
+
+        idx_cos_end = hbm_dae_good.fourier.N_HBM + 1
+        idx_sin_start = hbm_dae_bad.fourier.N_HBM + 1
+        idx_sin_end = hbm_dae_bad.fourier.N_HBM + hbm_dae_good.fourier.N_HBM + 1
+
+        idx_low_harmos = np.r_[0:idx_cos_end, idx_sin_start:idx_sin_end]
+        X_bad = X_bad[:, idx_low_harmos]
+
+        idx_low_harmos_n = np.r_[
+            0 : dae_good.n_dof * idx_cos_end,
+            dae_good.n_dof * idx_sin_start : dae_good.n_dof * idx_sin_end,
+        ]
+        hill_matrix_bad = hill_matrix_bad[np.ix_(idx_low_harmos_n, idx_low_harmos_n)]
+
+    else:
+        idx_start = hbm_dae_bad.fourier.N_HBM - hbm_dae_good.fourier.N_HBM
+        idx_end = hbm_dae_bad.fourier.N_HBM + hbm_dae_good.fourier.N_HBM + 1
+        X_bad = X_bad[:, idx_start:idx_end]
+
+        hill_matrix_bad = hill_matrix_bad[
+            dae_good.n_dof * idx_start : dae_good.n_dof * idx_end,
+            dae_good.n_dof * idx_start : dae_good.n_dof * idx_end,
+        ]
+
+    axs[2].bar(
+        xvals_freqs_good,
+        np.linalg.norm(X_good - X_bad, ord=2, axis=0),
+    )
+    axs[2].set_yscale("log")
+    axs[2].set_title("Difference")
+
+    plt.figure()
+    plt.title("Hill's matrices difference norm")
+    plt.imshow(np.abs(hill_matrix_good - hill_matrix_bad), cmap="viridis")
+    plt.colorbar(label="Absolute difference")
 
 
 if __name__ == "__main__":
