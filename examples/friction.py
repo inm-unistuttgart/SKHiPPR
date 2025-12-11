fix a few bugs in pedulum definition, but system isn't solved due to singular jacobian

examples/friction.py
@@ -27,7 +27,7 @@ def plot_frc():
     masses = [1, 1]
     g = 9.81
     stiffnesses = [1, 1]
-    dampings = [1, 1]
+    dampings = [0.05, 0.05]
     forcings = [0, 0.5]
     omega = 1.15
     phases = [0, 0]
@@ -51,13 +51,14 @@ def plot_frc():
 
     fourier = Fourier(N_HBM=N_HBM, L_DFT=1000, n_dof=dae.n_dof, real_formulation=True)
 
-    hbm = HBMEquation(
+    hbm = HBMEquationDAE(
         dae,
         omega,
         fourier=fourier,
-        initial_guess=np.zeros(dae.n_dof * (2 * fourier.N_HBM + 1))
-        + 1e-3 * np.random.rand(dae.n_dof * (2 * fourier.N_HBM + 1)),
-        stability_method=KoopmanHillSubharmonic(fourier, tol=1e-4, autonomous=False),
+        initial_guess=np.random.rand(dae.n_dof * (2 * fourier.N_HBM + 1)),
+        stability_method=KoopmanHillDAE(
+            fourier, tol=1e-4, autonomous=False, tol_drazin=1e-6
+        ),
     )
 
     solver.solve_equation(hbm, unknown="X")

skhippr/odes/daes.py
@@ -215,8 +215,8 @@ class FrictionOscillator(AbstractDAE):
         stiffnesses = np.append(stiffnesses, 0)
         dampings = np.append(dampings, 0)
 
-        self.K = np.empty_like(M_small)
-        self.D = np.empty_like(M_small)
+        self.K = np.zeros((len(masses), len(masses)))
+        self.D = np.zeros(self.K.shape)
         for i in range(len(masses)):
 
             self.K[i, i] = stiffnesses[i + 1] + stiffnesses[i]
@@ -235,36 +235,36 @@ class FrictionOscillator(AbstractDAE):
             [[np.zeros_like(self.K), np.eye(len(masses))], [-self.K, -self.D]]
         )
 
-        self.lam_crit = self.mu * self.masses[-1] * g
+        self.lam_crit = self.mu * masses[-1] * g
 
-        @property
-        def q(self):
-            return self.x[: len(self.stiffnesses), ...]
+    @property
+    def q(self):
+        return self.x[: len(self.stiffnesses), ...]
 
-        @q.setter
-        def q(self, value):
-            self.x[: len(self.stiffnesses), ...] = value
+    @q.setter
+    def q(self, value):
+        self.x[: len(self.stiffnesses), ...] = value
 
-        @property
-        def q_dot(self):
-            return self.x[len(self.stiffnesses) : -1, ...]
+    @property
+    def q_dot(self):
+        return self.x[len(self.stiffnesses) : -1, ...]
 
-        @q_dot.setter
-        def q_dot(self, value):
-            self.x[len(self.stiffnesses) : -1, ...] = value
+    @q_dot.setter
+    def q_dot(self, value):
+        self.x[len(self.stiffnesses) : -1, ...] = value
 
-        @property
-        def lam(self):
-            return np.atleast_1d(self.x[-1, ...])
+    @property
+    def lam(self):
+        return np.atleast_1d(self.x[-1, ...])
 
-        @lam.setter
-        def lam(self, value):
-            self.x[-1, ...] = value
+    @lam.setter
+    def lam(self, value):
+        self.x[-1, ...] = value
 
     def forcing(self, t=None):
         if t is None:
             t = self.t
-        F = np.zeros((len(self.stiffnesses), *np.atleast_1d(t).shape), dtype=t.dtype)
+        F = np.zeros((self.n_dof, *np.atleast_1d(t).shape))
         for i in range(len(self.stiffnesses)):
             F[len(self.stiffnesses) + i, ...] = self.forcing_amplitudes[i] * np.sin(
                 t + self.forcing_phases[i]
@@ -281,14 +281,14 @@ class FrictionOscillator(AbstractDAE):
         self.check_dimensions(t, x)
 
         g = (
-            self.q_dot[-1, ...]
-            + np.min(
+            x[-2, ...]
+            + np.minimum(
                 0,
-                self.prox_parameter * (self.lam + self.lam_crit) - self.q_dot[-1, ...],
+                self.prox_parameter * (x[-1, ...] + self.lam_crit) - x[-2, ...],
             )
-            + np.max(
+            + np.maximum(
                 0,
-                self.prox_parameter * (self.lam - self.lam_crit) - self.q_dot[-1, ...],
+                self.prox_parameter * (x[-1, ...] + self.lam_crit) - x[-2, ...],
             )
         )
         return g
@@ -302,8 +302,9 @@ class FrictionOscillator(AbstractDAE):
         self.check_dimensions(t, x)
 
         f = np.zeros_like(x)
-        f[:-1, ...] = self.jacobian_ode @ x[:-1, ...] + self.forcing(t)
+        f[:-1, ...] = self.jacobian_ode @ x[:-1, ...]
         f[-1, ...] = self.constraint(t, x)
+        f += self.forcing(t)
 
         return f
 