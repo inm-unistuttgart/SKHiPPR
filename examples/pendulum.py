ec2288281b4591df984e37fa2544e30cf31825db
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-01-21 13:29:53 +0100

    generate usable pendulum frc for paper

--- examples/pendulum.py
+++ examples/pendulum.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+import tikzplotlib
 
 warnings.filterwarnings("error", category=np.exceptions.ComplexWarning)
 
@@ -22,20 +23,20 @@ from skhippr.stability.KoopmanHillProjection import (
 
 def plot_single_solution():
 
-    solver_newton = NewtonSolver(tolerance=1e-8, max_iterations=50, verbose=True)
-    solver = ScipyFsolveSolver(
-        tolerance=1e-8, max_iterations=1000, verbose=True, use_fprime=True
-    )
+    solver = NewtonSolver(tolerance=1e-8, max_iterations=50, verbose=True)
+    # solver = ScipyFsolveSolver(
+    #     tolerance=1e-8, max_iterations=1000, verbose=True, use_fprime=True
+    # )
 
     m = 1
-    g = 9.81
+    g = 10
     l = 1.0
-    d = 0.05
+    d = 0.1
     F = 0.5
     omega = 1.15
     phi = 0.0
 
-    N_HBM = 15
+    N_HBM = 10
 
     # ODE formulation
     ode = PendulumODE(m, d, g, l, F, omega, phi)
@@ -43,7 +44,7 @@ def plot_single_solution():
 
     # HBM systems
     fourier_ode = Fourier(
-        N_HBM=N_HBM, L_DFT=2000, n_dof=ode.n_dof, real_formulation=True
+        N_HBM=N_HBM, L_DFT=1024, n_dof=ode.n_dof, real_formulation=True
     )
     hbm_ode = HBMEquation(
         ode,
@@ -73,7 +74,7 @@ def plot_single_solution():
         ]
     )
 
-    fourier_dae = Fourier(N_HBM=15, L_DFT=2000, n_dof=dae.n_dof, real_formulation=True)
+    fourier_dae = Fourier(N_HBM=15, L_DFT=1024, n_dof=dae.n_dof, real_formulation=True)
     hbm_dae = HBMEquationDAE(
         dae,
         omega,
@@ -100,17 +101,22 @@ def plot_single_solution():
     fig, axs = plt.subplots(4, 2)
 
     for i in range(4):
-        axs[i][0].plot(t, x_dae_init[i, :])
-        axs[i][0].plot(t, x_dae_solved[i, :], "--")
+        plt.figure()
+        plt.plot(t, x_dae_init[i, :])
+        plt.plot(t, x_dae_solved[i, :], "--")
+        plt.legend(["ODE", "DAE"])
+        tikzplotlib.save(f"plots/pendulum_timeseries_state_{i}.tikz")
 
-        axs[i][1].semilogy(t, np.abs(x_dae_init[i, :] - x_dae_init[i, :]))
-        axs[i][1].semilogy(t, np.abs(x_dae_init[i, :] - x_dae_solved[i, :]), "--")
+        plt.figure()
+        plt.semilogy(t, np.abs(x_dae_init[i, :] - x_dae_init[i, :]))
+        plt.semilogy(t, np.abs(x_dae_init[i, :] - x_dae_solved[i, :]), "--")
+        tikzplotlib.save(f"plots/pendulum_timeseries_error_{i}.tikz")
 
-    axs[0][0].set_title(
-        f"Pendulum ODE (solid) vs. DAE N = {hbm_dae.fourier.N_HBM} (dashed)"
-    )
+    # axs[0][0].set_title(
+    #     f"Pendulum ODE (solid) vs. DAE N = {hbm_dae.fourier.N_HBM} (dashed)"
+    # )
 
-    axs[0][0].set_title(f"Error between DAE solution and ODE solution")
+    # axs[0][0].set_title(f"Error between DAE solution and ODE solution")
 
     fig_FM, ax = plt.subplots(nrows=1, ncols=1)
     phis = np.linspace(0, 2 * np.pi, 250)
@@ -119,6 +125,7 @@ def plot_single_solution():
     ax.plot(np.real(hbm_dae.eigenvalues), np.imag(hbm_dae.eigenvalues), "+")
     ax.set_aspect("equal", "box")
     ax.set_title(f"Floquet multipliers pendulum N_HBM = {hbm_dae.fourier.N_HBM}")
+    tikzplotlib.save("plots/pendulum_Floquet_multipliers.tikz")
 
 
 def plot_frc():
@@ -185,7 +192,10 @@ def plot_frc():
         ),
     )
 
+    omegas = [[], [], []]
+    amps = [[], [], []]
     fig, axs = plt.subplots(1, 2)
+
     for idx, hbm in enumerate([hbm_ode, hbm_dae]):
 
         sys = EquationSystem(
@@ -202,9 +212,22 @@ def plot_frc():
             verbose=True,
         ):
             if branch_point.stable:
+                if len(omegas[1]) == 0:
+                    stb = 0
+                else:
+                    stb = 2
                 color = "r"
             else:
+                stb = 1
                 color = "b"
+
+            if idx == 1:
+
+                omegas[stb].append(branch_point.omega)
+                amps[stb].append(
+                    np.max(np.abs(branch_point.equations[0].x_time()[0, :]))
+                )
+
             axs[idx].plot(
                 branch_point.omega,
                 np.max(np.abs(branch_point.equations[0].x_time()[0, :])),
@@ -214,12 +237,22 @@ def plot_frc():
             if branch_point.omega > 4:
                 break
 
-    axs[0].set_title("Pendulum ODE FRC")
-    axs[0].set_xlabel("omega")
-    axs[0].set_ylabel("phi max")
-    axs[1].set_title("Pendulum DAE FRC")
-    axs[1].set_xlabel("omega")
-    axs[1].set_ylabel("y max")
+    # axs[0].set_title("Pendulum ODE FRC")
+    # axs[0].set_xlabel("omega")
+    # axs[0].set_ylabel("phi max")
+    # tikzplotlib.save("plots/pendulum_frc_ode")
+
+    plt.figure()
+    colors = ["r", "b", "r"]
+
+    for om, amp, col in zip(omegas, amps, colors):
+        plt.plot(om, amp, col)
+
+    plt.title("Pendulum DAE FRC")
+    plt.xlabel("omega")
+    plt.ylabel("y max")
+
+    tikzplotlib.save("plots/pendulum_frc_dae")
 
 
 if __name__ == "__main__":
