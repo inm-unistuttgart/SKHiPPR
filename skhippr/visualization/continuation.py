fc7112be7acdd0ffc82f48de863381d7e50a4edd
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-03-04 13:39:00 +0100

    Small fix and formatting in visualization/continuation.py (case where a HBMEquation has stability_method=None)

--- skhippr/visualization/continuation.py
+++ skhippr/visualization/continuation.py
@@ -11,19 +11,22 @@ import numpy as np
 
 from skhippr.solvers.continuation import BranchPoint
 
+
 def plot_continuation(
     branch: Iterable[BranchPoint],
-    plot_fun : Callable[[BranchPoint], Iterable[float] | float] = lambda bp: bp.vector_of_unknowns[0],
-    ax = None,
+    plot_fun: Callable[
+        [BranchPoint], Iterable[float] | float
+    ] = lambda bp: bp.vector_of_unknowns[0],
+    ax=None,
     **plot_kwargs
-    ):
+):
     """
     Plot the numerical continuation results stored in a branch of :py:class:`~skhippr.solvers.continuation.BranchPoint` objects.
     This function visualizes one-parameter continuation data by plotting a scalar or vector measure defined by ``plot_fun`` against the continuation parameter (or iteration index if no parameter is defined).
     If stability information is available for all :py:class:`~skhippr.solvers.continuation.BranchPoint` objects contained in ``branch``, stable segments will be drawn in red, unstable ones in blue. Otherwise, the entire branch is colored black.
-    
+
     The plot automatically adapts to one-, two-, or three-dimensional measures returned by ``plot_fun``.
-    
+
     Parameters
     ----------
     branch : Iterable[BranchPoint]
@@ -52,27 +55,35 @@ def plot_continuation(
     """
     branch_list = list(branch)
 
-    stability_defined = all(
-        getattr(bp, "equation_determining_stability", None) is not None
-        for bp in branch_list
-    )
+    stability_defined = True
+    for bp in branch_list:
+        if (
+            getattr(bp, "equation_determining_stability", None) is None
+            or bp.equation_determining_stability.stability_method is None
+        ):
+            stability_defined = False
+            break
 
     values, dim = _get_values_and_dimension(branch_list, plot_fun)
-    if dim not in (1,2,3):
-        raise ValueError("plot_fun must return a scalar or a numpy array of size 2 or 3.")
+    if dim not in (1, 2, 3):
+        raise ValueError(
+            "plot_fun must return a scalar or a numpy array of size 2 or 3."
+        )
 
     generated_ax = False
     if ax is None:
         if dim == 3:
             fig = plt.figure()
-            ax = fig.add_subplot(111, projection = "3d")
+            ax = fig.add_subplot(111, projection="3d")
         else:
             _, ax = plt.subplots(1, 1)
         generated_ax = True
 
     parameter = branch_list[0].equations[-1].continuation_parameter
     if parameter is not None:
-        parameter_values = np.array([np.squeeze(getattr(bp, parameter)) for bp in branch_list])
+        parameter_values = np.array(
+            [np.squeeze(getattr(bp, parameter)) for bp in branch_list]
+        )
     else:
         parameter_values = np.arange(len(branch_list))
 
@@ -93,7 +104,7 @@ def plot_continuation(
     if dim == 1:
         xs, ys = parameter_values, values[:, 0]
     elif dim == 2:
-        xs, ys = values[:,0], values[:,1]
+        xs, ys = values[:, 0], values[:, 1]
     elif dim == 3:
         xs, ys, zs = values[:, 0], values[:, 1], values[:, 2]
     xs_stable = np.where(stable_flags, xs, np.nan)
@@ -101,7 +112,9 @@ def plot_continuation(
 
     if dim == 3:
         ax.plot3D(xs_stable, ys, zs, color=stable_col, label="stable", **plot_kwargs)
-        ax.plot3D(xs_unstable, ys, zs, color=unstable_col, label="unstable", **plot_kwargs)
+        ax.plot3D(
+            xs_unstable, ys, zs, color=unstable_col, label="unstable", **plot_kwargs
+        )
     else:
         ax.plot(xs_stable, ys, color=stable_col, label="stable", **plot_kwargs)
         ax.plot(xs_unstable, ys, color=unstable_col, label="unstable", **plot_kwargs)
@@ -117,30 +130,31 @@ def plot_continuation(
 
     return ax
 
+
 def _to_array(value: float | np.ndarray | Iterable):
     """
     Convert different numeric inputs into a flat :py:class:`numpy.ndarray`.
     This function handles scalars, arrays, lists and tuples by squeezing each element individually when iterating over containers.
     It succeeds when all elements in ``value`` squeeze to the same length using ``np.squeeze(element)``.
-    
+
     Parameters
     ----------
     value: float | np.ndarray | Iterable
         Input to be converted to 1D array. It accepts:
-        
-        - Scalars and arrays with dimension 0: ``1.0`` or ``1`` as well as ``np.array(1.0)`` 
-        - Single-element containers: ``[1.0]``, ``(1.0,)``, ``np.array([1.0])``, ``np.array([[1.0]])`` 
+
+        - Scalars and arrays with dimension 0: ``1.0`` or ``1`` as well as ``np.array(1.0)``
+        - Single-element containers: ``[1.0]``, ``(1.0,)``, ``np.array([1.0])``, ``np.array([[1.0]])``
         - Containers where all first level elements contained squeeze to the same length: ``[[1.0]]``, ``[(1.0,)]``, ``1.0``.
-    
+
     Returns
     --------
     arr : numpy.ndarray
         1D :py:class:`numpy.ndarray` of shape ``(n,)`` where ``n`` is the common squeezed length of all input elements.
-    
+
     Notes
     -----
     ``value`` may contain different combinations of each for each element, as long as `np.squeeze(element)` always returns the same shape.
-    
+
     """
     if np.isscalar(value):
         return np.atleast_1d(value)
@@ -150,20 +164,21 @@ def _to_array(value: float | np.ndarray | Iterable):
         return np.ravel(value)
     return np.array([np.squeeze(v) for v in value])
 
+
 def _get_values_and_dimension(
     branch: Iterable[BranchPoint],
     plot_fun: Callable[[BranchPoint], object],
-    ):
+):
     """
     Evaluate ``plot_fun`` across branch points and validate consistent dimensionality
-    
+
     Parameters
     ----------
     branch : Iterable[BranchPoint]
         Continuation branch containing instances of class :py:class:`~skhippr.solvers.continuation.BranchPoint`.
     plot_fun : Callable[[BranchPoint], object]
         Measure function returning values accepted by :py:func:`~skhippr.visualization.continuation._to_array`.
-    
+
     Returns
     -------
     values : numpy.ndarray
