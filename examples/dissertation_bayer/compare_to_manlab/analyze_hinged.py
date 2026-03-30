a94712ba9030beb136b43c99e3b1647e402f454b
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-03-30 10:25:16 +0200

    hinged-hinged step 1 ready

--- examples/dissertation_bayer/compare_to_manlab/analyze_hinged.py
+++ examples/dissertation_bayer/compare_to_manlab/analyze_hinged.py
@@ -10,7 +10,12 @@ from skhippr.odes.manlab import HingedHinged
 from skhippr.Fourier import Fourier
 
 # --- Stability method ---
-from skhippr.stability.KoopmanHillProjection import KoopmanHillSubharmonic
+from skhippr.stability.KoopmanHillProjection import (
+    KoopmanHillSubharmonic,
+    KoopmanHillProjection,
+)
+from skhippr.stability.ClassicalHill import ClassicalHill
+from skhippr.stability.SinglePass import SinglePassRK4
 
 # --- Continuation + solver ---
 from skhippr.solvers.continuation import pseudo_arclength_continuator
@@ -27,8 +32,12 @@ from skhippr.visualization.data_export import save_tikz
 # --- Reference solution creation ---
 from create_reference import iterate_reference_solution
 
+# --- Analyze the system ---
+from analyze_and_plot import *
+
 
 def main():
+    ### TESTING ######
     # ode = init_hinged(2, 0.005, 0.1)
     # frc = compute_frc(
     #     ode,
@@ -38,12 +47,25 @@ def main():
     #     omega_max_normalized=1.8,
     #     max_stepsize=0.4,
     # )
-    frc = create_hinged_reference(n_modes=3, N_HBM=10, L_DFT=1024)
-    ax = plot_continuation(frc, plot_fun=plot_fun)
 
-    save_tikz(
-        axes=ax,
-        filepath=f"hinged_hinged_ref_N_{frc[0].equations[0].fourier.N_HBM}.tikz",
+    #### CREATE REFERENCE SOLUTION ######
+    # frc = create_hinged_reference(n_modes=3, N_HBM=10, L_DFT=1024)
+    # ax = plot_continuation(frc, plot_fun=plot_fun)
+
+    # save_tikz(
+    #     axes=ax,
+    #     filepath=f"hinged_hinged_ref_N_{frc[0].equations[0].fourier.N_HBM}.tikz",
+    # )
+
+    ### STEP 1: Analyze FM errors for different stability methods and HBM orders ###
+    ax, error_stats = iterate_step_1(
+        n_modes=10,
+        xi=0.005,
+        Nmax=40,
+        Ns_HBM=range(1, 39),
+        shooting_tol=1e-14,
+        labels_stabmethod=("dir", "subh", "imag", "RK4"),
+        early_break=np.inf,
     )
 
     plt.show()
@@ -138,6 +160,16 @@ def plot_fun(bp, x_eval=0.75):
     return (omega_normalized, np.max(np.abs(w)))
 
 
+def FM_error_measure(FMs, FMs_ref):
+    FMs_ref = FMs_ref[np.imag(FMs_ref) >= 0]
+    idx_ref = np.argmax(np.abs(FMs_ref))
+    return np.min(np.abs(FMs_ref[idx_ref] - FMs))
+
+
+def get_filename(ode, N_HBM, shooting_tol):
+    return f"examples/dissertation_bayer/compare_to_manlab/hinged_nmodes_{ode.n_modes}_N_{N_HBM}_tol_{shooting_tol}.csv"
+
+
 def create_hinged_reference(n_modes=3, N_HBM=10, L_DFT=1024):
     solver = NewtonSolver(tolerance=1e-13, verbose=False)
     ode = init_hinged(n_modes=n_modes, xi_0=0.005, omega_0_normalized=0.1)
@@ -150,7 +182,7 @@ def create_hinged_reference(n_modes=3, N_HBM=10, L_DFT=1024):
     frc_ref = []
 
     for bp in iterate_reference_solution(
-        filename=f"examples/dissertation_bayer/compare_to_manlab/hinged_nmodes_{ode.n_modes}_N_{fourier.N_HBM}_tol_{shooting_tol}.csv",
+        filename=get_filename(ode, N_HBM, shooting_tol),
         initial_system=hbm,
         solver=solver,
         stepsize=0.1,
@@ -167,6 +199,72 @@ def create_hinged_reference(n_modes=3, N_HBM=10, L_DFT=1024):
 
         frc_ref.append(bp)
     return frc_ref
+
+
+def step_1(
+    n_modes,
+    xi,
+    Nmax=40,
+    Ns_HBM=None,
+    shooting_tol=1e-14,
+    stability_method_generator=KoopmanHillSubharmonic,
+    early_break=np.inf,
+):
+    if Ns_HBM is None:
+        Ns_HBM = range(1, Nmax)
+    ode = init_hinged(n_modes=n_modes, xi_0=xi, omega_0_normalized=0.1)
+    filename = get_filename(ode=ode, N_HBM=Nmax, shooting_tol=shooting_tol)
+
+    error_stats = compute_step_1(
+        ode=ode,
+        filename=filename,
+        Ns_HBM=Ns_HBM,
+        L_DFT=1024,
+        stability_method_generator=stability_method_generator,
+        solver=NewtonSolver(tolerance=shooting_tol, verbose=False),
+        early_break=early_break,
+    )
+
+    ax = plot_step_1(error_stats)
+    return ax, error_stats, filename
+
+
+def iterate_step_1(
+    n_modes,
+    xi,
+    Nmax=120,
+    Ns_HBM=None,
+    shooting_tol=1e-14,
+    labels_stabmethod=("subh",),
+    early_break=np.inf,
+):
+    for label_stab in labels_stabmethod:
+        match label_stab:
+            case "subh":
+                stability_method_generator = KoopmanHillSubharmonic
+            case "dir":
+                stability_method_generator = KoopmanHillProjection
+            case "imag":
+                stability_method_generator = lambda fourier: ClassicalHill(
+                    fourier, "imaginary"
+                )
+            case "RK4":
+                stability_method_generator = SinglePassRK4
+            case _:
+                raise ValueError(f"Unknown stability method {label_stab}")
+
+        ax, error_stats, filename = step_1(
+            n_modes=n_modes,
+            xi=xi,
+            Nmax=Nmax,
+            Ns_HBM=Ns_HBM,
+            shooting_tol=shooting_tol,
+            stability_method_generator=stability_method_generator,
+            early_break=early_break,
+        )
+        ax.set_title(f"step 1 FM errors for {label_stab}")
+        tikzplotlib.save(f"{filename.split('.')[0]}_stab_{label_stab}.tikz")
+    return ax, error_stats
 
 
 if __name__ == "__main__":
