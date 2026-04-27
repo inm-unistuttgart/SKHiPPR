21b39e376bad7bf8146f253fb0ef28da1dbe0924
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-04-27 11:59:36 +0200

    adjust the csv export script

--- doctrees/HillML/generate_reference.py
+++ doctrees/HillML/generate_reference.py
@@ -1,31 +1,44 @@
 from skhippr.Fourier import Fourier
+from skhippr.cycles.hbm import HBMEquation
 
 
 def init_csv(fourier: Fourier, writer, name_param: str):
 
-    errors = [
+    params = [
         name_param,
         "arclength",
-        "shoot error init",
-        "shoot error max",
-        "shoot error FMs",
     ]
-    X_labels = [f"X 0, {l}" for l in range(fourier.n_dof)]
+    X_labels = [f"X0 [{l}]" for l in range(fourier.n_dof)]
+    J_labels = []
+    for l in range(fourier.n_dof):
+        for j in range(fourier.n_dof):
+            J_labels = J_labels + [f"J0 [{j},{l}]"]
 
+    # CAUTION - J counts in Fortran-style, i.e., across the columns first, not lexicographically!
+    # This makes it more difficult to read, but it is more efficient to write and read from the csv file.
     if fourier.real_formulation:
         for sc in ["c", "s"]:
             for k in range(1, fourier.N_HBM + 1):
-                X_labels = X_labels + [f"X{sc} {k}, {l}" for l in range(fourier.n_dof)]
+                X_labels = X_labels + [f"X{sc},{k} [{l}]" for l in range(fourier.n_dof)]
 
+                for l in range(fourier.n_dof):
+                    for j in range(fourier.n_dof):
+                        J_labels = J_labels + [f"J{sc},{k}, [{j}],{l}]"]
     else:
         for k in range(1, fourier.N_HBM + 1):
             X_labels = X_labels + [f"X {k}, {l}" for l in range(fourier.n_dof)]
             X_labels = [f"X {-k}, {l}" for l in range(fourier.n_dof)] + X_labels
 
-    FM_labels = [f"FM {k}" for k in range(fourier.n_dof)]
+            for l in range(fourier.n_dof):
+                for j in range(fourier.n_dof):
+                    J_labels = J_labels + [f"J,{k}, [{l}],{j}]"]
+                    J_labels = [f"J,{-k}, [{j}],{l}]"] + J_labels
 
-    writer.writerow(errors + FM_labels + X_labels)
-    return errors + FM_labels + X_labels
+    FM_labels = [f"FM [{k}]" for k in range(fourier.n_dof)]
+
+    header = params + FM_labels + X_labels + J_labels
+    writer.writerow(header)
+    return header
 
 
 def to_csv(
@@ -33,24 +46,12 @@ def to_csv(
     hbm: HBMEquation,
     name_param,
     arclength,
-    solver,
-    FM_error_measure=None,
-    **kwargs_odesolver,
 ):
     _, FMs = hbm.determine_stability(update=True)
     param = getattr(hbm, name_param)
     X = hbm.X
-    errors, _ = determine_ode_accuracy(
-        hbm,
-        solver,
-        visualize=False,
-        FM_error_measure=FM_error_measure,
-        **kwargs_odesolver,
-    )
-    # errors = (0, 0, 0)
-    row = np.hstack(
-        (np.atleast_1d(param), np.atleast_1d(arclength), np.atleast_1d(errors), FMs, X),
-        dtype=complex,
-    )
+    hill_mat = hbm.hill_matrix(real_formulation=True, update=True)
+    Js = hbm.fourier.matrix_inv_DFT(hill_mat)
 
-    writer.writerow([str(complex(val)).strip("()") for val in row])
+    row = [param, arclength] + list(FMs) + list(X) + list(Js.flatten(order="F"))
+    writer.writerow(row)
