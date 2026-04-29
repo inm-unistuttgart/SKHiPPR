3f330a91d58967dc74957f8ea85aee2d3d9d4913
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-04-29 15:30:42 +0200

    new file just for plotting

--- examples/dissertation_bayer/friction/friction.py
+++ examples/dissertation_bayer/friction/friction.py
@@ -21,12 +21,7 @@ from skhippr.stability.KoopmanHillProjection import (
 from friction_init import init_oscillator, get_description
 from friction_direct import solve_friction
 from drazin import plot_drazin_and_ratio
-
-from skhippr.visualization.cycles import (
-    plot_period,
-    plot_phase,
-    plot_floquet_multipliers,
-)
+from plot_friction import plot_hbm_result
 
 
 def solve_hbm(
@@ -134,40 +129,6 @@ def iterate_over_N(
         else:
             hbm_ref = hbm
             yield hbm
-
-
-def plot_and_save_solution(hbm, description, idx=(0, 1), path_export=None):
-    ax = None
-    for i in idx:
-        ax = plot_phase(hbm, idx=i, ax=ax, label=f"x_{i}")
-    ax.set_title(f"{description}")
-    ax.legend()
-    if path_export is not None:
-        tikzplotlib.save(f"{path_export}{description}.tikz")
-
-
-def plot_hbm_result(hbm, description, path="plots/"):
-    path = "plots/"
-    r = np.linalg.norm(hbm.residual(update=False))
-
-    # position, velocity, lambda in three plots
-    ax_pos = plot_and_save_solution(hbm, f"position_{description}", (0, 1), path)
-    ax_vel = plot_and_save_solution(hbm, f"velocity_{description}", (0, 1), path)
-    ax_force = plot_and_save_solution(hbm, f"lambda_{description}", (0, 1), path)
-
-    # force law
-    ax_forcelaw = plot_phase(hbm, idx=(3, 4))
-    ax_forcelaw.set_title(f"force_law_{description}")
-
-    ax_forcelaw.set_xlabel("x3")
-    ax_forcelaw.set_ylabel("lambda")
-    ax_forcelaw.set_title(f"force law {description} r = {r}")
-    ax_forcelaw.legend()
-    tikzplotlib.save(f"{path}forcelaw_{description}.tikz")
-
-    return ax_pos, ax_vel, ax_force, ax_forcelaw
-
-    # np.savetxt(f"X_{description}.csv", hbm.X, delimiter=";")
 
 
 def plot_and_save(hbms, Ns_HBM, description, tol_drazin=1e-7):
