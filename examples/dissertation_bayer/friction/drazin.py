1199a086ab0fd8167340ea575204ebc3ae6e69b6
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-04-29 14:54:03 +0200

    extract Drazin plotting function

--- examples/dissertation_bayer/friction/drazin.py
+++ examples/dissertation_bayer/friction/drazin.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import tikzplotlib
 
 from scipy.linalg import lu_factor, lu_solve
 
 from skhippr.stability.KoopmanHillProjection import drazin
-
 from skhippr.cycles.hbm import HBMEquationDAE
 
 
@@ -36,3 +37,42 @@ def compute_drazin_ratio(
     _, ratio = drazin(pencil_M, tol_drazin, ax_plot=ax)
     # print(f"Ratio of Drazin inverse: {ratio}")
     return ratio
+
+
+def plot_drazin_and_ratio(
+    hbms,
+    tol_drazin,
+    description,
+    ratio_limits=(),
+    ax_drazin=None,
+    ax_ratio=None,
+    path_drazin=None,
+    path_ratio=None,
+):
+
+    drazin_ratios = []
+    Ns_HBM = []
+
+    # Drazin inverse analysis + plotting for all HBMs
+    for k, hbm in enumerate(hbms):
+        drazin_ratios[k] = compute_drazin_ratio(hbm, ax_drazin, tol_drazin=tol_drazin)
+        Ns_HBM[k] = hbm.fourier.N_HBM
+
+    if ax_drazin is not None:
+
+        ax_drazin.axhline(tol_drazin, linestyle="--")
+        ax_drazin.set_xlabel("n*(2*N+1)")
+        ax_drazin.set_ylabel("magnitude of eigenvalue")
+        ax_drazin.set_title(f"Drazin eigenvalues {description}")
+
+        if path_drazin is not None:
+            tikzplotlib.save(f"{path_drazin}_drazin_{description}.tikz")
+
+    if ax_ratio is not None:
+        ax_ratio.plot(Ns_HBM, drazin_ratios, "-x")
+        for limit in ratio_limits:
+            ax_ratio.axhline(limit, linestyle="--")
+        ax_ratio.set_title(f"Drazin ratio {description}")
+
+        if path_ratio is not None:
+            tikzplotlib.save(f"{path_ratio}_drazinratio_{description}.tikz")
