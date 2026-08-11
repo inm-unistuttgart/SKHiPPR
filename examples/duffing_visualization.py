64927f7eec8965829325202297a50df520bd26be
Fabia Bayer <bayer@inm.uni-stuttgart.de>
2026-08-11 15:53:25 +0200

    add the continuation visualization to duffing_visualization

--- examples/duffing_visualization.py
+++ examples/duffing_visualization.py
@@ -20,14 +20,21 @@ from skhippr.stability.KoopmanHillProjection import KoopmanHillSubharmonic
 # --- Solver ---
 from skhippr.solvers.newton import NewtonSolver
 
+# --- Continuation ---
+from skhippr.solvers.continuation import pseudo_arclength_continuator
+
 # --- Visualization ---
 from skhippr.visualization.cycles import (
     plot_period,
     plot_phase,
     plot_floquet_multipliers,
+    animate_floquet_multipliers,
     plot_floquet_exponents,
+    animate_floquet_exponents,
     plot_hill_matrix_blocks,
 )
+from skhippr.visualization.continuation import plot_continuation
+
 from skhippr.visualization.data_export import (
     save_png,
     save_pdf,
@@ -60,7 +67,7 @@ def visualize_single_solution(hbm_sys: HBMSystem, save=False):
     omega = hbm_sys.equations[0].omega
 
     # --- Plotting a HBMSystem directly will use the first valid HBMEquation contained in the system ---
-    plot_phase(
+    ax_phase = plot_phase(
         hbm=hbm_sys,
         title=f"Phase plot (Duffing, omega = {omega}), HBMSystem passed",
         xlabel="x_0",
@@ -69,7 +76,7 @@ def visualize_single_solution(hbm_sys: HBMSystem, save=False):
 
     # --- Alternatively, a specific HBMEquation can be extracted from the system and passed to the plotting function(s) ---
     hbm_equation = hbm_sys.equations[0]
-    plot_phase(
+    ax_phase_equ = plot_phase(
         hbm=hbm_equation,
         title=f"Phase plot (Duffing, omega = {omega}), HBMEquation passed",
         xlabel="x_0",
@@ -79,24 +86,24 @@ def visualize_single_solution(hbm_sys: HBMSystem, save=False):
     )
 
     # --- Same for the period in time
-    plot_period(
+    ax_period = plot_period(
         hbm=hbm_sys,
         n_periods=1.22,
         title=f"Time series (Duffing, omega = {omega})",
     )
 
     # --- Stability measures ---
-    plot_floquet_multipliers(
+    ax_FM = plot_floquet_multipliers(
         hbm=hbm_sys,
         title=f"Floquet multipliers (Duffing, omega = {omega}), HBMSystem passed",
     )
-    plot_floquet_exponents(
+    ax_FE = plot_floquet_exponents(
         hbm=hbm_sys,
-        title=f"Floquet multipliers (Duffing, omega = {omega}), HBMSystem passed",
+        title=f"Floquet exponents (Duffing, omega = {omega}), HBMSystem passed",
     )
 
     # --- Visualize the Hill matrix ---
-    plot_hill_matrix_blocks(
+    ax_hill_matrix = plot_hill_matrix_blocks(
         hbm=hbm_sys, real_formulation=None, logscale=True, cmap="plasma"
     )
 
@@ -105,16 +112,76 @@ def visualize_single_solution(hbm_sys: HBMSystem, save=False):
 
     # --- Realisation with subplots ---
     _, axs = plt.subplots(nrows=2, ncols=2)
-    axs[0] = plot_phase(hbm=hbm_sys, ax=axs[0])
-    axs[1] = plot_floquet_multipliers(hbm=hbm_sys, ax=axs[1])
-    axs[1].axis("equal")
-    axs[2] = plot_floquet_exponents(hbm=hbm_sys, ax=axs[2])
+    plot_phase(hbm=hbm_sys, ax=axs[0][0])
+    plot_floquet_multipliers(hbm=hbm_sys, ax=axs[0][1])
+    axs[0][1].axis("equal")
+    plot_floquet_exponents(hbm=hbm_sys, ax=axs[1][0])
 
-    # --- Save the hill matrix visualization plot ---
-    # The relative path for saving a file can be given if the filepath string starts without a "/".
-    # Forward slashes "/" can be used regardless of operating system.
-    # save_pdf(axes=ax, filepath="plots/duffing_plots/hill_matrix.pdf")
-    # save_png(axes=ax, filepath="plots/duffing_plots/hill_matrix.png")
+    if save:
+        # --- Save the hill matrix visualization plot ---
+        # The relative path for saving a file can be given if the filepath string starts without a "/".
+        # Forward slashes "/" can be used regardless of operating system.
+        save_pdf(axes=ax_hill_matrix, filepath="plots/duffing_plots/hill_matrix.pdf")
+        save_png(axes=ax_hill_matrix, filepath="plots/duffing_plots/hill_matrix.png")
+
+
+def visualize_continuation_result(frc):
+    # --- Plot the continuation curve using SKHiPPR visualization functions---
+
+    # for the continuation plot, define the 1-3 scalar quantities plotted against each other
+    def plot_fun_1d(branch_point):
+        # return the maximum of the first unknown in time domain. If only one parameter is returned, the continuation param appears on the x axis.
+        return np.max(branch_point.equations[0].x_time()[0, :])
+
+    def plot_fun_2d(branch_point):
+        # return the maximum of the first unknown in time domain and the continuation parameter. If two parameters are returned, they appear on the x and y axis in their respective order. This allows, for instance, to produce a rotated plot.
+        return (
+            np.max(branch_point.equations[0].x_time()[0, :]),
+            branch_point.omega,
+        )
+
+    def plot_fun_3d(branch_point):
+        # return the continuation parameter, the maximum of the first unknown in the time domain, and the amplitude of the first harmonic.If three parameters are returned, they appear on the x, y and z axis in their respective order.
+        return (
+            branch_point.omega,
+            np.max(branch_point.equations[0].x_time()[0, :]),
+            branch_point.X[branch_point.equations[0].fourier.n_dof],
+        )
+
+    ax_1d = plot_continuation(
+        frc,
+        plot_fun=plot_fun_1d,
+        title="Continuation curve (Duffing, 1D plot)",
+        xlabel="omega",
+        ylabel="max(x_0(t))",
+    )
+
+    ax_2d = plot_continuation(
+        frc,
+        plot_fun=plot_fun_2d,
+        title="Flipped Continuation curve (Duffing, 2D plot)",
+        xlabel="max(x_0(t))",
+        ylabel="omega",
+    )
+
+    ax_3d = plot_continuation(
+        frc,
+        plot_fun=plot_fun_3d,
+        title="Continuation curve (Duffing, 3D plot)",
+        xlabel="omega",
+        ylabel="max(x_0(t))",
+        zlabel="X_1",
+    )
+
+
+def animate_continuation_result(frc):
+    # --- Animate the Floquet multipliers and exponents along the continuation branch ---
+    _, animation1 = animate_floquet_multipliers(hbm_set=frc)
+    _, animation2 = animate_floquet_exponents(hbm_set=frc)
+    return (
+        animation1,
+        animation2,
+    )
 
 
 def main():
@@ -125,8 +192,8 @@ def main():
     solver = NewtonSolver(verbose=True)
 
     # --- Setup parameters ---
-    omega = 0.8
-    F = 0.05
+    omega = 0.3
+    F = 0.5
 
     # --- Instantiation of the ODE at initial point ---
     ode = Duffing(t=0, x=[1.0, 0.0], alpha=1, beta=2, delta=0.16, F=F, omega=omega)
@@ -151,10 +218,38 @@ def main():
 
     visualize_single_solution(hbm_sys, save=False)
 
-    # --- Saving using save_tikz requires tikzplotlib to be installed which is imported locally ---
-    # save_tikz(axes=ax, filepath="duffing_plots/hill_matrix.tex")
+    # --- Continuation and visualize ---
+
+    # --- Preallocate the result of the FRC continuation ---
+    # BranchPoints (a subclass of EquationSystem) extend the initial EquationSystem with one added equation for the tangency condition.
+    frc = []
+
+    # it is a good idea to turn off the solver verbosity before starting a continuation
+    solver.verbose = False
+
+    # --- Iterate through the branch. ---
+    for branch_point in pseudo_arclength_continuator(
+        initial_system=hbm_sys,
+        solver=solver,
+        stepsize=0.1,
+        stepsize_range=(0.001, 0.1),
+        continuation_parameter="omega",
+        initial_direction=1,
+        verbose=True,
+        num_steps=4000,
+    ):
+        frc.append(branch_point)
+
+        # break if omega exceeds maximum
+        if branch_point.omega > 2.5:
+            break
+
+    visualize_continuation_result(frc)
+    animations = animate_continuation_result(frc)
+
+    return animations
 
 
 if __name__ == "__main__":
-    main()
+    animations = main()
     plt.show()
